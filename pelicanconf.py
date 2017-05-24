3a64d661728e54d22544a5435bf60e0a4307c7c8
Joao Moreira <joao.moreira@u.northwestern.edu>
2017-05-24 18:49:14 -0500

    Fix atom feeds by converting all urls to absolutes

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,7 +36,6 @@ DISPLAY_PAGES_ON_MENU = True
 LOAD_CONTENT_CACHE = False
 
 # Feed generation is usually not desired when developing
-FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://jagmoreira.github.io'
+SITEURL = 'http://jagmoreira.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
